7fdd200dfa77be5f39d8b34345725fa72d4e6fa5
Patrick Tung <tung_patrick@yahoo.com>
2019-09-11 15:53:58 -0700

    ready for deploy

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ plt.style.use("tableau-colorblind10")
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.config.suppress_callback_exceptions = True
+app.css.config.serve_locally = True
+app.scripts.config.serve_locally = True
 
 # data
 df = pd.read_csv("data/data.csv")
